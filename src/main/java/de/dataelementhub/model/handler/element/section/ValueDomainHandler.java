3315aa2d6b0336c75625e1a8c8b10f35e89725dd
Michael Folz <folz@med.uni-frankfurt.de>
2022-06-29 15:49:18 +0200

    #46 - Released enumeratedValueDomains should have at least one permittedValue
    - Throw illegal argument exception when trying to create an enumerated value domain without permissible values (when the new state is RELEASED)

--- src/main/java/de/dataelementhub/model/handler/element/section/ValueDomainHandler.java
+++ src/main/java/de/dataelementhub/model/handler/element/section/ValueDomainHandler.java
@@ -142,6 +142,13 @@ public class ValueDomainHandler extends ElementHandler {
       throw new IllegalAccessException("User has no write access to namespace.");
     }
 
+    if (valueDomain.getType().equals(ValueDomain.TYPE_ENUMERATED)
+        && valueDomain.getIdentification().getStatus() == Status.RELEASED && (
+        valueDomain.getPermittedValues() == null || valueDomain.getPermittedValues().isEmpty())) {
+      throw new IllegalArgumentException(
+          "Can't create released enumerated value domain without permitted values.");
+    }
+
     final boolean autoCommit = CtxUtil.disableAutoCommit(ctx);
     de.dataelementhub.dal.jooq.tables.pojos.Element element = convert(valueDomain);
 
